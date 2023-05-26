ee45f7d18949e3cc02f01613639f4886017a3925
SmashedFrenzy16 <xbop.grillion@gmail.com>
2023-05-26 14:24:23 +0100

    Add files via upload

---
+++ WebBrowser/Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace WebBrowser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.backButton = new System.Windows.Forms.Button();
+            this.forwardButton = new System.Windows.Forms.Button();
+            this.homeButton = new System.Windows.Forms.Button();
+            this.searchButton = new System.Windows.Forms.Button();
+            this.webBrowser = new System.Windows.Forms.WebBrowser();
+            this.refreshButton = new System.Windows.Forms.Button();
+            this.stopButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(684, 64);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(1236, 31);
+            this.textBox.TabIndex = 0;
+            // 
+            // backButton
+            // 
+            this.backButton.Location = new System.Drawing.Point(22, 49);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(110, 60);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "←";
+            this.backButton.UseVisualStyleBackColor = true;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // forwardButton
+            // 
+            this.forwardButton.Location = new System.Drawing.Point(152, 49);
+            this.forwardButton.Name = "forwardButton";
+            this.forwardButton.Size = new System.Drawing.Size(112, 60);
+            this.forwardButton.TabIndex = 2;
+            this.forwardButton.Text = "→";
+            this.forwardButton.UseVisualStyleBackColor = true;
+            this.forwardButton.Click += new System.EventHandler(this.forwardButton_Click);
+            // 
+            // homeButton
+            // 
+            this.homeButton.Location = new System.Drawing.Point(562, 49);
+            this.homeButton.Name = "homeButton";
+            this.homeButton.Size = new System.Drawing.Size(116, 60);
+            this.homeButton.TabIndex = 3;
+            this.homeButton.Text = "⌂";
+            this.homeButton.UseVisualStyleBackColor = true;
+            this.homeButton.Click += new System.EventHandler(this.homeButton_Click);
+            // 
+            // searchButton
+            // 
+            this.searchButton.Location = new System.Drawing.Point(1926, 49);
+            this.searchButton.Name = "searchButton";
+            this.searchButton.Size = new System.Drawing.Size(124, 60);
+            this.searchButton.TabIndex = 4;
+            this.searchButton.Text = "🔍";
+            this.searchButton.UseVisualStyleBackColor = true;
+            this.searchButton.Click += new System.EventHandler(this.searchButton_Click);
+            // 
+            // webBrowser
+            // 
+            this.webBrowser.Location = new System.Drawing.Point(0, 129);
+            this.webBrowser.MinimumSize = new System.Drawing.Size(20, 20);
+            this.webBrowser.Name = "webBrowser";
+            this.webBrowser.Size = new System.Drawing.Size(2060, 899);
+            this.webBrowser.TabIndex = 5;
+            // 
+            // refreshButton
+            // 
+            this.refreshButton.Location = new System.Drawing.Point(281, 49);
+            this.refreshButton.Name = "refreshButton";
+            this.refreshButton.Size = new System.Drawing.Size(116, 60);
+            this.refreshButton.TabIndex = 6;
+            this.refreshButton.Text = "↻";
+            this.refreshButton.UseVisualStyleBackColor = true;
+            this.refreshButton.Click += new System.EventHandler(this.refreshButton_Click);
+            // 
+            // stopButton
+            // 
+            this.stopButton.Location = new System.Drawing.Point(414, 49);
+            this.stopButton.Name = "stopButton";
+            this.stopButton.Size = new System.Drawing.Size(129, 60);
+            this.stopButton.TabIndex = 7;
+            this.stopButton.Text = "X";
+            this.stopButton.UseVisualStyleBackColor = true;
+            this.stopButton.Click += new System.EventHandler(this.stopButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(2062, 1028);
+            this.Controls.Add(this.stopButton);
+            this.Controls.Add(this.refreshButton);
+            this.Controls.Add(this.webBrowser);
+            this.Controls.Add(this.searchButton);
+            this.Controls.Add(this.homeButton);
+            this.Controls.Add(this.forwardButton);
+            this.Controls.Add(this.backButton);
+            this.Controls.Add(this.textBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Button forwardButton;
+        private System.Windows.Forms.Button homeButton;
+        private System.Windows.Forms.Button searchButton;
+        private System.Windows.Forms.WebBrowser webBrowser;
+        private System.Windows.Forms.Button refreshButton;
+        private System.Windows.Forms.Button stopButton;
+    }
+}
+
